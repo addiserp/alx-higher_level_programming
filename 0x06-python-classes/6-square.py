b1d0b31d8be08c86a880f1be38867af3ba650724
mikias gedlu <addiserp@gmail.com>
2023-01-06 17:56:08 +0300

    0x06-python-classes 6-square defines

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+# 0-square.py by Mikias Gedlu
+"""Defines a square """
+
+
+class Square:
+    """Does Define Square"""
+
+    def __init__(self, size=0):
+        """ a Module divides the numbers of a matrix
+        Args: Size - size of square
+        Returns:
+            Nothing
+        Raises:
+            TypeError : size must be an integer
+            ValueError : size must be >= 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Retrieves size of square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """
+        Prints area of the square in #
+        """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
+    @property
+    def position(self):
+        """property of the coordinates of this Square
+        Raises:
+            TypeError: if value != a tuple of 2 integers < 0
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """set the position of this Square
+        Args: value as a tuple of two positive integers
+        Raises:
+            TypeError: if value is not a tuple or any int in tuple < 0
+        """
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def pos_print(self):
+        """returns the position in spaces"""
+        pos = ""
+        if self.size == 0:
+            return "\n"
+        for w in range(self.position[1]):
+            pos += "\n"
+        for w in range(self.size):
+            for i in range(self.position[0]):
+                pos += " "
+            for j in range(self.size):
+                pos += "#"
+            pos += "\n"
+        return pos
