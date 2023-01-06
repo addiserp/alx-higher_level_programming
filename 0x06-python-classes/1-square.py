0eb20927f9f2d538b2d3ae14bf97ab0e5abc5a3e
mikias gedlu <addiserp@gmail.com>
2023-01-06 17:15:10 +0300

    0x06-python-classes 1-square defines

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 0-square.py by Mikias Gedlu
+"""Defines a square """
+
+
+class Square:
+    """Does nothing"""
+    pass
+    def Square(size):
+        """ a Module divides the numbers of a matrix
+        Args:
+        Size: Size of square
+        Returns:
+            Nothing
+        Raises:
+            Nothing
