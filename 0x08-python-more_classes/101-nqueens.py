24bfee29b039e3bcbf248f3b86794c99b31982c3
mikias gedlu <addiserp@gmail.com>
2023-01-06 18:45:38 +0300

    0x08. Python - More Classes and Objects : defines  bonus task

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle
-by mikias gedlu """
-
+"""Solves the N-queens puzzle"""
 
 import sys
 
 
 def init_board(n):
-    """Initialize chessboard an `n`x`n` sized chessboard with 0's."""
+    """Initialize an `n`x`n` sized chessboard with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
