92235416906dc4c74fc475fcbbba9a961521b175
mikias gedlu <addiserp@gmail.com>
2023-01-06 17:08:31 +0300

    0x06-python-classes hot fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ this Function test a class square with empty """
-class square():
-    
-    def square:
+class Square:
     """ this Function test a class square with empty #
     pass
