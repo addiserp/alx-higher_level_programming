7c4048bd1bbd749715d06a6684b78e89ef953b9e
mikias gedlu <addiserp@gmail.com>
2023-01-06 17:32:35 +0300

    0x06-python-classes 2-square defines

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,7 +15,7 @@ class Square:
             TypeError : size must be an integer
             ValueError : size must be >= 0
         """
-        
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
