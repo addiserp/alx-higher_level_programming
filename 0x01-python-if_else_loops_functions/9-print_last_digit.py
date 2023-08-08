9dc2d160ba8178d9d45d8ffb9d7a30e53092d19e
Mikias Gedlu <addiserp@gmail.com>
2023-08-08 19:04:41 +0300

    0x01-python-if_else_loops_functions 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-#a function that prints the last digit of a number
+# a function that prints the last digit of a number
 
 def print_last_digit(n):
     if n < 0:
-        l = n % -10
+        lst = n % -10
     else:
-        l = n % 10
-    i = abs(l)
+        lst = n % 10
+    i = abs(lst)
     print("{}".format(i), end="")
     return i
