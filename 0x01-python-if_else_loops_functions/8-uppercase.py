7c80ec6065bdc0970bd321bf1f61c4f85f4541af
Mikias Gedlu <addiserp@gmail.com>
2023-08-08 18:55:07 +0300

    0x01-python-if_else_loops_functions 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# change to Uppercase 
+def uppercase(str):
+    for i in range(len(str)):
+        j = ord(str[i])
+        if j >= 97 and j <= 122:
+            j = j - 32
+        print("{}".format(chr(j)), end="")
+    print()
