a9231a4f8424faa507310214276a9da8ba6e2121
Mikias Gedlu <addiserp@gmail.com>
2023-08-10 18:53:45 +0300

    0x02-python-import_modules 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if strcont == 0:
         print("0 arguments.")
     elif strcont == 1:
-        print("1 arguments.")
+        print("1 argument.")
     else:
         print("{} arguments:".format(strcont))
     for i in range(strcont):
