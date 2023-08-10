9a0cd0609c46375a11ef674cb044f79f24e7caf1
Mikias Gedlu <addiserp@gmail.com>
2023-08-10 19:23:14 +0300

    0x02-python-import_modules 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
+    from hidden_4 import *
 
     listname = dir(hidden_4)
     for i in listname:
         if i[:2] != "__":
             print(i)
-            
