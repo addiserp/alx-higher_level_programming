b7bf910f8274da7ad78cc6b7f2d16aa9b0423819
Mikias Gedlu <addiserp@gmail.com>
2023-08-10 19:00:51 +0300

    0x02-python-import_modules 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# a program that prints the result of the addition of all arguments
+if __name__ == "__main__":
+    import sys
+    strcont = len(sys.argv) - 1
+    sum = 0
+    for i in range(strcont):
+        sum += int(sys.argv[i + 1])
+    print(sum)
