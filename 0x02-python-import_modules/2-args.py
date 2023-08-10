d927f4c2da0491c0e60ea847c205a7ea53558c03
Mikias Gedlu <addiserp@gmail.com>
2023-08-10 18:45:21 +0300

    0x02-python-import_modules 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#a program that prints the number of and the list of its arguments.
+
+if __name__ == "__main__":
+    import sys
+    strcont = len(sys.argv) - 1
+    if strcont == 0:
+        print("0 arguments.")
+    elif strcont == 1:
+        print("1 arguments.")
+    else:
+        print("{} arguments:".format(strcont))
+    for i in range(strcont):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
