249af1b97bac38fe1491409c11e2dec4a6ec4c16
Mikias Gedlu <addiserp@gmail.com>
2023-08-12 19:58:59 +0300

    0x03-python-data_structures 2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,9 @@
 # 0x03-python-data_structures
 # a function that retrieves an element from a list like in C.
 def element_at(my_list, idx):
-    if idx < 0 :
-        return
+    if idx < 0:
+        return "None"
     elif idx > len(my_list) - 1:
-        return
+        return "None"
     else:
         return my_list[idx]
-    
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # 0x03-python-data_structures
-# a function that replaces an element of a list at a specific position (like in C).
+# a function that replaces an element of a list at
+# a specific position (like in C).
 def replace_in_list(my_list, idx, element):
-    if idx < 0 :
+    if idx < 0:
         return my_list
     elif idx > len(my_list) - 1:
         return my_list
     else:
         my_list[idx] = element
         return my_list
-    
