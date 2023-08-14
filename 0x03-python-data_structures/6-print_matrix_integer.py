3a7efdf0e0b271df9322ab24306cd63995529c75
Mikias Gedlu <addiserp@gmail.com>
2023-08-14 17:00:43 +0300

    0x03-python-data_structures 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # a function that prints a matrix of integers.
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix[:]:
-        for j in matrix[:]:
-            print("{:d} ".format([i][j]))
+    for i in matrix:
+        for j in i:
+            if j == i[-1]:
+                print('{:d}'.format(j), end='')
+            else:
+                print('{:d}'.format(j), end=' ')
+        print()
