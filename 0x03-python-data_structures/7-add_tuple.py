1eb1917c6fb1b06863d190b422cae7d6677fddd1
Mikias Gedlu <addiserp@gmail.com>
2023-08-14 17:42:26 +0300

    0x03-python-data_structures 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,5 +14,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new_b = tuple_b[0], tuple_b[1]
     else:
         new_b = 0, 0
-    new = new_a[0] + new_a[1], new_b[0] + new_b[1]
+    new = new_a[0] + new_b[0], new_a[1] + new_b[1]
     return new
