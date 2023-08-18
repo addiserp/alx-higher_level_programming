1fb246ec2663073ce9e037e9f543f39ce1ed38e2
Mikias Gedlu <addiserp@gmail.com>
2023-08-18 14:48:20 +0300

    0x03 hot fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     size = len(my_list)
     new_list = []
     if size == 0:
-        return my_list
+        return new_list
     for i in range(size):
         if my_list[i] % 2 == 0:
             new_list.append(True)
