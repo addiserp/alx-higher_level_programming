307b0adec1234634c02f11e350f1ed619354f70d
Mikias Gedlu <addiserp@gmail.com>
2023-08-19 16:18:15 +0300

    0x04-python-more_data_structures 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,29 +4,11 @@ def roman_to_int(roman_string):
         return 0
     if not isinstance(roman_string, str) or not roman_string.isupper():
         return 0
-    r_dict = {
-            "I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10,
-            "L": 50, "C": 100, "D": 500, "M": 1000}
-    outp = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                outp += v
-    return outp
+    rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    val = 0
+    for i in range(len(roman_string)):
+        if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
+            val += rom_val[roman_string[i]] - 2 * rom_val[roman_string[i - 1]]
+        else:
+            val += rom_val[roman_string[i]]
+    return val
