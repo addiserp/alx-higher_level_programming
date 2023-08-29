738854fa4ab9c366e5e3971c92faeaf3799bd936
Mikias Gedlu <addiserp@gmail.com>
2023-08-29 19:45:54 +0300

    0x05-python-exceptions 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# a function that
+def list_division(my_list_1, my_list_2, list_length):
+    divs = []
+    for index in range(list_length):
+        val = 0
+        try:
+            val = my_list_1[index] / my_list_2[index]
+        except ZeroDivisionError:
+            print('division by 0')
+        except TypeError:
+            print('wrong val type')
+        except IndexError:
+            print('val out of range')
+        finally:
+            divs.append(val)
+
+    return divs
