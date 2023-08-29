4e428c0759154677c52a2c793c7cd77871652043
Mikias Gedlu <addiserp@gmail.com>
2023-08-29 20:17:55 +0300

    0x06-python-classes 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 1-square.py by Mikias Gedlu
+"""Defines a square """
+
+class Square:
+    """Does Define Square"""
+    def __init__(self, size):
+        """ a Module divides the numbers of a matrix
+        Args: Size - size of square
+        Returns:
+            Nothing
+        Raises:
+            Nothing
+        """
+        self.__size = size
