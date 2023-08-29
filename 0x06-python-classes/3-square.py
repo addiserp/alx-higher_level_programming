ecdeae8ec01f3574db8f880610b95dd9c27965cb
Mikias Gedlu <addiserp@gmail.com>
2023-08-29 20:19:03 +0300

    0x06-python-classes 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 3-square.py by Mikias Gedlu
+"""Defines a square """
+
+class Square:
+    """Does Define Square"""
+
+    def __init__(self, size=0):
+        """ a Module divides the numbers of a matrix
+        Args: Size - size of square
+        Returns:
+            Nothing
+        Raises:
+            TypeError : size must be an integer
+            ValueError : size must be >= 0
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+        return (self.__size ** 2)
