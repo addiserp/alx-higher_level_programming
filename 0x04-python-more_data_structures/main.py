5f07313e044043c8efb6d9ecf848793a92c57712
Mikias Gedlu <addiserp@gmail.com>
2023-08-29 20:02:51 +0300

    0x05-python-exceptions update

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+ 
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+roman_number = "LXXXIX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
