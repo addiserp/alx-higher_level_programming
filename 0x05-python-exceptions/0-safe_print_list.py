ded63a9656d087109916597dd58360a742a51579
Mikias Gedlu <addiserp@gmail.com>
2023-08-29 19:14:06 +0300

    0x05-python-exceptions 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             print("index Error")
             break
-    print(" ")
-    return(result)
+    print()
+    return (result)
