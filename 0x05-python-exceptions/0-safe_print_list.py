b79263b183c4df74e96edb6778d2139d386e5ad1
Mikias Gedlu <addiserp@gmail.com>
2023-08-31 17:26:06 +0300

    hot fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             result += 1
         except IndexError:
-            print("index Error")
             break
         except Exception:
             break
