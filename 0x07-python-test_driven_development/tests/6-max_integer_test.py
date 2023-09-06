ebfcaea6686302a5edbab045010ce1d03f54e2f3
Mikias Gedlu <addiserp@gmail.com>
2023-09-06 14:37:28 +0300

    hot fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-"""a test Module for 6-max_integer.py to find the max integer in a list
+"""Unittest for max_integer([..])
 """
-
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
-class Testmax_integer(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     def test_MaxIinteger(self):
-        # test max integer when integers
+        # Test starts with all integers
         self.assertAlmostEqual(max_integer([1, -2, 3, 4]),4)
         self.assertAlmostEqual(max_integer([1, 5, -3, 4]),5)
         self.assertAlmostEqual(max_integer([8, 5, 3, 4]),8)
-
+    
+  #def test_values(self):
+        # Make sure value errors are raised when necessory
+   #     self.assertRaises(ValueError,max_integer, [1, -2, 3, 4.7]) '''
     def test_Types(self):
-        # make sure value errors are raized
-        self.assertRaises(TypeError, max_integer, [1, 2, 3, 2.4, "a"])
-
+        # Make sure value errors are raised when necessory
         self.assertRaises(TypeError,max_integer, [1, -2, 3, 4.7])
         self.assertRaises(TypeError,max_integer, [1, -2, 3, True])
         self.assertRaises(TypeError,max_integer, [1, -2, 3, "abc"])
