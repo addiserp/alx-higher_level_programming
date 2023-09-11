929219a12989a524f103edd8701240be0601b7c3
Mikias Gedlu <addiserp@gmail.com>
2023-09-11 18:24:03 +0300

    0x0A-python-inheritance 100fix

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """a class MyInt that inherits from int"""
 
+
 class MyInt(int):
     """a class MyInt that inherits from int"""
-    
+
     def __eq__(self, value):
         """change opeartor"""
         return self.real != value
