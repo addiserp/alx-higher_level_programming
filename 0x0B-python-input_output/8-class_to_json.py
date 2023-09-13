399e77ba9f3af6242fd9917e0251ba6707845c78
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 18:31:37 +0300

    0x0B-python-input_output 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,7 +2,6 @@
 """a function that returns the dictionary description with simple
 data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object:"""
-import json
 
 
 def class_to_json(obj):
