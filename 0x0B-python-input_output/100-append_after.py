24975f8d65c85a664fad3ad4a8e78e1d8492dbea
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 18:59:25 +0300

    0x0B-python-input_output 101

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,10 +7,10 @@ def append_after(filename="", search_string="", new_string=""):
     """a function that inserts a line of text to a file
     """
     mystr = ""
-    with open(filename, mode="+w", encoding="utf-8") as myfile:
+    with open(filename, mode="r", encoding="utf-8") as myfile:
         for line in myfile:
             mystr += line
             if search_string in line:
                 mystr += new_string
-    with open(filename, mode="+w", encoding="utf-8") as myfile:
-        myfile.write(mystr)
+    with open(filename, mode="w", encoding="utf-8") as myfile2:
+        myfile2.write(mystr)
