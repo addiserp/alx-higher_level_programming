fe98e81a073a6dd224e0a38b1be4efb8578e3507
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 18:42:17 +0300

    0x0B-python-input_output 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a class Student that defines a student by
+"""
+
+
+class Student:
+    """ a class Student that defines a student by
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        """__str__ documentation"""
+        v = "{} - {} - {:d}".format(self.first_name, self.last_name, self.age)
+        return "[Student] " + v
+
+    def to_json(self):
+        """__str__ to_json"""
+        return self.__dict__
