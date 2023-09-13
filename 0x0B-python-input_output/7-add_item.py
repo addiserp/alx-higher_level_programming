5be85c9c1193060d90b556ca5ddb2e04b3709a99
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 18:13:26 +0300

    0x0B-python-input_output 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,9 @@ import sys
 from os import path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-filename = "add_item.json"
 
+
+filename = "add_item.json"
 strcont = len(sys.argv) - 1
 if strcont == 0:
     sys.exit(1)
