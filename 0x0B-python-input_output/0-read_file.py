20e17d700fa46474395642ceee3055efad03e8b4
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 16:50:22 +0300

    0x0B-python-input_output 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,10 @@
 """a function that reads a text file (UTF8)
 and prints it to stdout:"""
 
+
 def read_file(filename=""):
-    with open(filename, mode='r',encoding="utf-8") as myfile:
-        return myfile.read()
+    """a function that reads a text file (UTF8)"""
+
+    with open(filename, mode='r', encoding="utf-8") as myfile:
+        myread = myfile.read()
+        print(myread)
