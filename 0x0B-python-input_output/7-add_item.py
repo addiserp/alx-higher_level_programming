b40f80e12f594c871b379b010846f74db7384f2c
Mikias Gedlu <addiserp@gmail.com>
2023-09-13 18:11:50 +0300

    0x0B-python-input_output 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to a Python list
+, and then save them to a file:"""
+import json
+import sys
+from os import path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+
+strcont = len(sys.argv) - 1
+if strcont == 0:
+    sys.exit(1)
+else:
+    my_list = []
+    for i in range(strcont):
+        my_list.append(sys.argv[i + 1])
+
+if path.isfile(filename):
+    curr_list = load_from_json_file(filename)
+    save_to_json_file(curr_list + my_list, filename)
+else:
+    save_to_json_file(my_list, filename)
