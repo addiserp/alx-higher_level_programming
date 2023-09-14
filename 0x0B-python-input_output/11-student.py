05a8e809903a5ba30ca7f128cdc394af651d23f7
Mikias Gedlu <addiserp@gmail.com>
2023-09-14 13:38:19 +0300

    0x0B-python-input_output 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" a class Student that defines a student by
+using 10-student.py
+"""
+
+
+class Student:
+    """ a class Student that defines a student by
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def __str__(self):
+        """__str__ documentation"""
+        v = "{} - {} - {:d}".format(self.first_name, self.last_name, self.age)
+        return "[Student] " + v
+
+    def to_json(self, ats=None):
+        """__str__ to_json"""
+        if (type(ats) == list and
+                all(type(ele) == str for ele in ats)):
+            return {k: getattr(self, k) for k in ats if hasattr(self, k)}
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        """ function script for reload_from_json"""
+
+        for k, v in json.items():
+            setattr(self, k, v)
