9c32e3e02ec748eed469ed1f479c45efcafceb9c
Natnael-Gedlu <nategd800@gmail.com>
2023-10-18 18:02:36 +0300

    0x0E-SQL_more_queries 3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,2 @@
 -- force_name in the current database in your MySQL server.
-CREATE table IF NOT EXISTS hbtn_0d_2.force_name(`id` INT, `name` VARCHAR(256) NOT NULL);
+CREATE table IF NOT EXISTS hbtn_0d_2.force_name(`id` INT, `name` VARCHAR(256));
