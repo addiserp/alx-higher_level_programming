8cba0b4844de61300a5754776359c8b9aa941995
Natnael-Gedlu <nategd800@gmail.com>
2023-10-18 18:25:44 +0300

    0x0E-SQL_more_queries 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- a script that creates database and table to the MySQL server user user_0d_1.
 CREATE database IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE table IF NOT EXISTS states(`id` INT NOT NULL AUTO_INCREMENT UNIQUE  PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
