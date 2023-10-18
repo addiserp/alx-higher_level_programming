547bded7bf5e052c717f03463f9e6a94ddc089b4
Natnael-Gedlu <nategd800@gmail.com>
2023-10-18 18:01:58 +0300

    0x0E-SQL_more_queries 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- id_not_null in the current database in your MySQL server.
-CREATE table IF NOT EXISTS hbtn_0d_2.id_not_null(`id` INT DEFAULT 1, `name` VARCHAR(256));
+CREATE table IF NOT EXISTS hbtn_0d_2.id_not_null(`id` INT NOT NULL DEFAULT 1, `name` VARCHAR(256));
