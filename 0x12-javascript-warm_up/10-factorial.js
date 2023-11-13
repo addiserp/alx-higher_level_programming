2c8f9a26cdcd4db03fdb45ba49ea804506b49c4b
Mikias Gedlu <addiserp@gmail.com>
2023-11-13 18:51:43 +0300

    0x12-javascript-warm_up 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const myFac = parseInt(process.argv[2]);
+if (isNaN(myFac)) {
+console.log('1');
+} else {
+console.log(factorial(myFac));
+}
+function factorial (f) {
+if (f <= 0) {
+    return 0;
+} else if (f === 1) {
+    return 1;
+} else {
+    return (f * factorial(f - 1));
+}
+}
