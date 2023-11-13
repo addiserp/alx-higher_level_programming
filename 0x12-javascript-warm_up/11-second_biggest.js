37487051fbb39e0a5a780fb7528e8f2bf034a4fe
Mikias Gedlu <addiserp@gmail.com>
2023-11-13 19:12:34 +0300

    0x12-javascript-warm_up 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 if (process.argv.length < 4) {
-    console.log('0');
-  } else {
-    const size = process.argv.length;
-    const ints = [];
-    for (let ii = 2; ii < size; ii++) {
-      ints[ii - 2] = parseInt(process.argv[ii]);
-    }
-    ints.sort(function (a, b) { return b - a; });
-    console.log(ints[1]);
+  console.log('0');
+} else {
+  const size = process.argv.length;
+  const ints = [];
+  for (let ii = 2; ii < size; ii++) {
+    ints[ii - 2] = parseInt(process.argv[ii]);
   }
+  ints.sort(function (a, b) { return b - a; });
+  console.log(ints[1]);
+}
