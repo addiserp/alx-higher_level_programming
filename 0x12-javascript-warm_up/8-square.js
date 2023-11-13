e69646c4251a42b2a5d8261ea44a7af760623ca1
Mikias Gedlu <addiserp@gmail.com>
2023-11-13 18:42:02 +0300

    0x12-javascript-warm_up 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+let i = 0;
+let j = 'x';
+
+if (process.argv[2] === undefined || isNaN(x)) {
+  console.log('Missing size');
+} else {
+    while (i < x - 1) {
+        j+= 'x';
+        i++;
+    }
+    while (i > 0) {
+        console.log(j);
+        i--;
+    }
+}
