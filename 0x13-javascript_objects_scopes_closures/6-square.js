092db00a596df26fd78dfaee97b06db213a88e16
Mikias Gedlu <addiserp@gmail.com>
2023-11-14 16:23:13 +0300

    0x13-javascript_objects_scopes_closures 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+// a class Square that defines a square and inherits from Square of 5-square.js:
+
+const OldSquare = require('./5-square');
+
+module.exports = class Square extends OldSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    super.double();
+  }
+
+  charPrint (c = 'X') {
+    super.print(c);
+  }
+};
