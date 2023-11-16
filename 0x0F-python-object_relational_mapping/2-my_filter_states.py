f925f6791fcfa8eec358b2fa4dd68e9889eda9fd
Mikias Gedlu <addiserp@gmail.com>
2023-11-16 18:46:57 +0300

    0. 0x0F-python-object_relational_mapping 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+#!/home/ad/venv/bin/python3 // Local test venv//
+a script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+conn = MySQLdb.connect(host="localhost", port=3306,
+                       user=argv[1], passwd=argv[2],
+                       db=argv[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name like '{}' ORDER BY id ASC"
+            .format(argv[4]))
+# HERE I have to know SQL to grab serched states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
