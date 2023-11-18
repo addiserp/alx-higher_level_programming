3917cb10a6c74963790b4fbb18832566f1754084
Mikias Gedlu <addiserp@gmail.com>
2023-11-18 18:20:49 +0300

    0. 0x0F-python-object_relational_mapping hotfix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
-
+#!/home/ad/venv/bin/python3
 """
-#!/home/ad/venv/bin/python3 // Local test venv//
+#!/usr/bin/python3
 a script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
@@ -14,15 +13,15 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=argv[1], passwd=argv[2],
-                       db=argv[3], charset="utf8")
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name like '{}' ORDER BY id ASC"
-            .format(argv[4]))
-# HERE I have to know SQL to grab serched states in my database
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=argv[1], passwd=argv[2],
+                           db=argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name like BINARY '{}'\
+                ORDER BY id ASC".format(argv[4]))
+    # HERE I have to know SQL to grab serched states in my database
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
