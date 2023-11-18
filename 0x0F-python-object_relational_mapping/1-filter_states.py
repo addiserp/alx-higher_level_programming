62b105ec908050ed17138b7702e6e715d9a08a53
Mikias Gedlu <addiserp@gmail.com>
2023-11-18 18:13:23 +0300

    0. 0x0F-python-object_relational_mapping hotfix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name like BINARY 'N%' ORDER BY id ASC")
     # HERE I have to know SQL to grab all states in my database
     query_rows = cur.fetchall()
     for row in query_rows:
