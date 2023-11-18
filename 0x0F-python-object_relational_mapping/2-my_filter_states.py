2feab1b3d016d2737787f07a99d1bbe28f51f66c
Mikias Gedlu <addiserp@gmail.com>
2023-11-18 18:28:44 +0300

    0. 0x0F-python-object_relational_mapping hotfix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,6 +5,7 @@ a script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
+
 import MySQLdb
 from sys import argv
 
@@ -17,7 +18,7 @@ if __name__ == '__main__':
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name like BINARY '{}'\
+    cur.execute("SELECT * FROM states WHERE name like BINARY '%{}%'\
                 ORDER BY id ASC".format(argv[4]))
     # HERE I have to know SQL to grab serched states in my database
     query_rows = cur.fetchall()
