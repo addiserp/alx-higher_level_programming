83316a48ca1090981bffe806b2e4eae14885609a
Mikias Gedlu <addiserp@gmail.com>
2023-11-18 18:38:38 +0300

    0. 0x0F-python-object_relational_mapping hotfix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
-#!/home/ad/venv/bin/python3
-"""
 #!/usr/bin/python3
-a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-
+This script takes in an argument and
+displays all values in the states
+where `name` matches the argument
+from the database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
 from sys import argv
@@ -14,16 +14,15 @@ if __name__ == '__main__':
     Access to the database and get the states
     from the database.
     """
-    conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2],
-                           db=argv[3], charset="utf8")
-    cur = conn.cursor()
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
     cur.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY '{}' \
                  ORDER BY states.id ASC".format(argv[4]))
-    # HERE I have to know SQL to grab serched states in my database
-    query_rows = cur.fetchall()
-    for row in query_rows:
+    rows = cur.fetchall()
+
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
