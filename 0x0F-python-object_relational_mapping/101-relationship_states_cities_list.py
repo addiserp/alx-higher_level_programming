a9fff515c81afa46e712d7d8c7dead9f82980fc2
Mikias Gedlu <addiserp@gmail.com>
2023-11-20 18:43:41 +0300

    0. 0x0F-python-object_relational_mapping 101 bonus

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,7 +9,7 @@ from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 
 
 if __name__ == "__main__":
@@ -21,11 +21,12 @@ if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3], pool_pre_ping=True)
     engine = create_engine(db_uri)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
+
     result_q = session.query(State).order_by(State.id)
+
     for state in result_q:
         print("{}: {}".format(state.id, city.name))
         for city in state.cities:
