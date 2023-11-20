1d8f9b382a48101c00c589e2bd0f820e4faf52fa
Mikias Gedlu <addiserp@gmail.com>
2023-11-20 18:40:21 +0300

    0. 0x0F-python-object_relational_mapping 101 bonus

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+  a script that lists all State objects, and corresponding City objects,
+  contained in the database hbtn_0e_101_usa
+"""
+
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    """
+    Access to the database and get a relationsip state
+    from the database 100.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3], pool_pre_ping=True)
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    result_q = session.query(State).order_by(State.id)
+    for state in result_q:
+        print("{}: {}".format(state.id, city.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+
+    session.close()
