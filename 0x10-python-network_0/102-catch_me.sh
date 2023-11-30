594c5a9146833c88b393ca23c8064b1fbab6738a
Mikias Gedlu <addiserp@gmail.com>
2023-11-30 19:52:51 +0300

    0x10. Python - Network #0 bonus3

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me
 curl - sX PUT - L - d "user_id=98" \
-        - H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
+- H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
