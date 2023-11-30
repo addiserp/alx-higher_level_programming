caa681721a0e9d1a41aad57ea6c8faa58e7f589e
Mikias Gedlu <addiserp@gmail.com>
2023-11-30 19:30:44 +0300

    0x10. Python - Network #0 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# a Bash script that sends a DELETE request to the
-# URL passed as the first argument and displays the
-# body of the response
-
+# This script sends a DELETE request to URL passed as the first argument
 curl - sX "DELETE" "$1"
