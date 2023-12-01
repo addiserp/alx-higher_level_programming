fe412074eda0079367893b676a6f55b88da1fc22
Mikias Gedlu <addiserp@gmail.com>
2023-12-01 17:57:46 +0300

    0x11. Python - Network #1 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,9 +10,8 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    RURL = argv[1]
     try:
-        with urllib.request.urlopen(RURL) as response:
+        with urllib.request.urlopen(argv[1]) as response:
             html = response.read()
             print(html.decode('UTF-8'))
     except urllib.error.URLError as e:
