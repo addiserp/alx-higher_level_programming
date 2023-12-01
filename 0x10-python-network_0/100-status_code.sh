2828b0bd78f4f3e8136bc48a09ee18e6e0d74961
Mikias Gedlu <addiserp@gmail.com>
2023-12-01 11:24:46 +0300

    0x10. Python - Network #0 hotfix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as
-curl - sLw " % { http_code } " - o / dev / null  "$1"
+curl -sLw " % { http_code } " -o / dev / null  "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a JSON POST request to a URL
-curl - sLw " % { http_code } " - o / dev / null  "$1"
+curl -sLw " % { http_code } " -o / dev / null  "$1"
