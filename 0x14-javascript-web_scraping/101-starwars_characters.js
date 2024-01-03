557865ca583b876790d5381aa3de3bfdb111e7fd
Mikias Gedlu <addiserp@gmail.com>
2024-01-03 18:43:23 +0300

    0x14. JavaScript - Web scraping bonus101 hostfix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,7 +7,7 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 function printActors (act, ix) {
   request(act[ix], (err0r, res, body) => {
     if (err0r) {
-        console.log('error:', err0r);
+      console.log('error:', err0r);
     } else {
       console.log(JSON.parse(body).name);
       if (ix + 1 < act.length) {
