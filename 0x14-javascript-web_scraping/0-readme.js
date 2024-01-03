317388f93fcd37fe0727399dee038f9bf7fd4a01
Mikias Gedlu <addiserp@gmail.com>
2024-01-03 16:36:50 +0300

    0x14. JavaScript - Web scraping 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
-#!/usr/bin/nodejs
-// a script that reads and prints.
+#!/usr/bin/node
+// a script that reads and prints the content of a file.
 
 const file = process.argv[2];
 const fs = require('fs');
