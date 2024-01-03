bf3f1ff9cd18957290ca615b5a6f379030faca89
Mikias Gedlu <addiserp@gmail.com>
2024-01-03 17:54:26 +0300

    0x14. JavaScript - Web scraping 5

--- 0x14-javascript-web_scraping/testhabesh.js
+++ 0x14-javascript-web_scraping/testhabesh.js
@@ -1,22 +1,10 @@
 #!/usr/bin/node
 // a script that prints the title of a Star Wars movie where
 // the episode number matches a given integer.
-const file = 'habeshtest.text';
+const file = 'habeshtest.html';
 const fsfun = require('fs');
 const request = require('request');
-const url = 'https://www.habeshatender.com/sessionuser.php?username=addiserp&password=addiserp';
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(body);
-    fsfun.writeFile(file, body, 'utf8', function (error) {
-        if (error) {
-          console.log(error);
-        }
-      });
-  }
-});
+const url = 'https://www.habeshatender.com/userloginpage.php?username=addiserp&password=addiserp';
 
 
-
+request(url).pipe(fsfun.createWriteStream(file));
