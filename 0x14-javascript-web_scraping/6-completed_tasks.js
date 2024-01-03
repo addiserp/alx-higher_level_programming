3d8a96728621ccccc0487ae2d2bba78b445c94b1
Mikias Gedlu <addiserp@gmail.com>
2024-01-03 18:07:57 +0300

    0x14. JavaScript - Web scraping 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// a script that computes the number of tasks completed by user id.
+
+const reque = require('request');
+const args = process.argv;
+const URL = args[2];
+
+reque(URL, function (error, response, body) {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const todo = JSON.parse(body);
+    const list = {};
+    for (let j = 0; j < todo.length; j++) {
+      const status = (todo[j]['completed']);
+      const k = todo[j]['userId'].toString();
+      if (status) {
+        if (list[k]) {
+          list[k]++;
+        } else {
+          list[k] = 1;
+        }
+      }
+    }
+    console.log(list);
+  }
+});
