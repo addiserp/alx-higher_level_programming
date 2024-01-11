b71804ff57ce1c55c94950e0a0c89a21fb61d439
Mikias Gedlu <addiserp@gmail.com>
2024-01-11 18:50:01 +0300

    0x15. JavaScript - Web jQuery 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,3 @@
+// a  JavaScript script that updates <header> element to red (#FF0000):
+const data = document.querySelector('header');
+data.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,5 @@
+## ALX Assignment
+0x15-javascript-web_jquery
+## Author
+Mikias Gedlu
+@addiserp.com
