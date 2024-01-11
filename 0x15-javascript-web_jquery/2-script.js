32eb009f5ffcbd3d60e35b4bdce80304b8b72e41
Mikias Gedlu <addiserp@gmail.com>
2024-01-11 18:59:31 +0300

    0x15. JavaScript - Web jQuery 2

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// using JQuery API on click
+// a  JavaScript script that updates <header> element to red (#FF0000):
+
+$('DIV#red_header').click(function () {
+    $('header').css({ color: '#FF0000' });
+});
