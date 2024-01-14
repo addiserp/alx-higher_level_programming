040c4b9ef96a396ccb8a0edcfdf061e0952c64cc
Mikias Gedlu <addiserp@gmail.com>
2024-01-14 18:02:42 +0300

    0x15. JavaScript - Web jQuery 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+// a JavaScript script that fetches the character name from
+// this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+    $('#character').html(data['name']);
+});
